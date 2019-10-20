1a2d3cc46013bf5033fb34193a52a73a4343847e
Martin Serantes <mssonfire@gmail.com>
2019-10-20 21:07:41 +0200

    working

--- JS/DOM_Eventos/www/js/index.js
+++ JS/DOM_Eventos/www/js/index.js
@@ -1,44 +1,53 @@
 "use strict";
 
-const primary = document.querySelector("section");
-console.log(primary);
+// const primary = document.querySelector("section");
+// console.log(primary);
 
-primary.firstElementChild.style.color = "steelblue";
-console.log(primary.firstElementChild);
-console.log(primary.childNodes);
+// primary.firstElementChild.style.color = "steelblue";
+// console.log(primary.firstElementChild);
+// console.log(primary.childNodes);
 
-const ps = document.querySelectorAll("p");
+// const ps = document.querySelectorAll("p");
 
-for (const p of ps) {
-  p.style.fontFamily = "arial";
-}
+// for (const p of ps) {
+//   p.style.fontFamily = "arial";
+// }
 
-const header = document.getElementsByTagName("header");
-console.log(header);
-console.log(header.childNodes);
+// const header = document.getElementsByTagName("header");
+// console.log(header);
+// console.log(header.childNodes);
 
 /* --------------------------------------------------------------- */
 
-/* const info = () => console.log(window.navigator.userAgent);
-console.log(info);
+// const info = () => console.log(window.navigator.userAgent);
+// console.log(info);
 
-const message = () => {
-  console.log(window.innerWidth);
-};
+// const message = () => {
+//   console.log(window.innerWidth);
+// };
 
-setTimeout(() => {
-  console.log(message);
-}, 3000); */
+// setTimeout(() => {
+//   console.log(message);
+// }, 3000);
 
-let num = 10;
+// let num = 10;
 
-const count = () => {
-  if (num > 0) {
-    console.log(num--);
-  } else {
-    console.log("se acabó el tiempo!!");
-    clearInterval(int);
-  }
-};
+// const count = () => {
+//   if (num > 0) {
+//     console.log(num--);
+//   } else {
+//     console.log("se acabó el tiempo!!");
+//     clearInterval(int);
+//   }
+// };
 
-const int = setInterval(count, 2000);
+// const int = setInterval(count, 2000);
+
+/*--------------------------------------------------*/
+
+const header = document.querySelector("header");
+const newParagraph = document.createElement("p");
+const newText = document.createTextNode("Este es un nuevo parrafo");
+
+newParagraph.appendChild(newText);
+header.appendChild(newParagraph);
